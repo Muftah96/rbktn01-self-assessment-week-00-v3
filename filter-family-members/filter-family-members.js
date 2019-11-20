2180548c359b96d8ff13127fd6bab93d9f2534d4
muftah <muftah.elzawi@gmail.com>
2019-11-20 10:53:10 +0200

    IDONTKNOW

--- filter-family-members/filter-family-members.js
+++ filter-family-members/filter-family-members.js
@@ -13,44 +13,76 @@
 //
 // EXAMPLE:
 //
-// var familyTree = {
-//   'firstName': 'Beth',
-//   'lastName': 'Johnson',
-//   'location': 'San Francisco',
-//   'children': [
-//     {
-//       'firstName': 'Beth Jr.',
-//       'lastName': 'Johnson',
-//       'location': 'Berkeley',
-//       'children': [
-//         {
-//           'firstName': 'Smitty',
-//           'lastName': 'Won',
-//           'location': 'Beijing',
-//           'children': []
-//         }
-//       ]
-//     },
-//     {
-//       'firstName': 'Joshie',
-//       'lastName': 'Wyattson',
-//       'location': 'Berkeley',
-//       'children': []
-//     }
-//   ]
-// };
+var familyTree = {
+  'firstName': 'Beth',
+  'lastName': 'Johnson',
+  'location': 'San Francisco',
+  'children': [
+    {
+      'firstName': 'Beth Jr.',
+      'lastName': 'Johnson',
+      'location': 'Berkeley',
+      'children': [
+        {
+          'firstName': 'Smitty',
+          'lastName': 'Won',
+          'location': 'Beijing',
+          'children': []
+        }
+      ]
+    },
+    {
+      'firstName': 'Joshie',
+      'lastName': 'Wyattson',
+      'location': 'Berkeley',
+      'children': []
+    }
+  ]
+};
 //
-// var livesInBerkeley = function (familyMember) {
-//   return familyMember.location === 'Berkeley';
-// }
+var livesInBerkeley = function (familyTree) {
+  return familyTree.location === 'Berkeley';
+}
 //
 // filterFamilyMembers(familyTree, livesInBerkeley)
 //
 // returns ['Beth Jr. Johnson', 'Joshie Wyattson'];
 
-
+// my idea to solve it is by going inside the object.
+// checking if it is an object if it is we go next.
+// checking for the truthTest.
+// onse it is true we push the first name and last name to an array.
+// then we check if the children is array od not.
+// if they are an array we call back the funcution.
+// the children array will go to the else we will loop in it checking also for truthTest.
+// if the test is true we push the first name and the last name.
+// but for a resoun i dont know the idea it dosent work.
 
 var filterFamilyMembers = function (familyTree, truthTest) {
   // All your code in this function body
+var arr = []
+if (!Array.isArray(familyTree)){
+
+	for (var key in familyTree) {
+		if (truthTest(familyTree[key])) {
+		   arr.push( familyTree.firstName + ' ' + familyTree.lastName )
+		}
+
+		if ( Array.isArray(familyTree['children'])) {
+			filterFamilyMembers(familyTree['children'],truthTest)
+		}
+
+	}
+}else{
+	for (var i = 0;	i < familyTree.length ; i++)	{
+		if (truthTest(familyTree[i])) {
+		arr.push( familyTree[i].firstName + ' ' + familyTree[i].lastName )
+		}
+	}
+}
+
+
+return arr
+
 };
 
